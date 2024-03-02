ef356781d01e1631cbb9773ffbbc7d0d86beac74
decaljoonho <161907483+decaljoonho@users.noreply.github.com>
2024-03-02 11:38:12 +0900

    24 3 2
    asdasdasdasdfsdafasdfasfd

---
+++ asdfghjkl.py
@@ -0,0 +1,39 @@
+##print("apple")
+##print("juice")
+##print("apple" "juice")
+##print("apple", "juice")
+##print("apple", "juice", sep = '~')
+##print("apple", "juice", end = ' is ')
+##print("tasty")
+##input("What is your favortie juice?")
+
+##print(input("What is your favorite juice? "))
+##print(input("이름 : "))
+##print(input("학년 : "))
+##print(input("반 : "))
+
+##print("A\nB\nC\nD\n")
+
+##import turtle
+##turtle.shape("turtle")
+##turtle.write("Welcome to Python World!", True, font = ("Arial", 20, "bold"))
+##turtle.circle(20, 150)
+##turtle.done()
+
+##import turtle
+##turtle.circle(100, steps = 6)
+##turtle.done()
+
+import turtle
+turtle.shape("triangle")
+turtle.circle(100)
+turtle.circle(150)
+turtle.circle(200)
+turtle.done()
+
+
+
+
+
+
+                

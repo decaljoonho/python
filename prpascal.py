3673126eaee9f0a2460ce8afd5a2a2266bf99dca
decaljoonho <161907483+decaljoonho@users.noreply.github.com>
2024-03-16 11:47:40 +0900

    prapscal
    asdf

---
+++ prpascal.py
@@ -0,0 +1,195 @@
+##import turtle
+##t = turtle.Turtle()
+##
+##t.setx(100)
+##t.sety(100)
+##t.setx(0)
+##t.sety(0)
+##
+##t.clear()
+##-----------------------------
+##import turtle
+##t = turtle.Turtle()
+##
+##t.goto(100, 0)
+##t.goto(100, 100)
+##t.goto(0, 100)
+##t.goto(0, 0)
+##
+##t.clear()
+##_________________________
+##import turtle
+##t = turtle.Turtle()
+##t.setheading(90)
+##t.forward(100)
+##t.setheading(180)
+##t.forward(100)
+##t.setheading(270)
+##t.forward(100)
+##t.home()
+##-------------------------------------
+##import turtle
+##t = turtle.Turtle()
+##t.goto(100, 0)
+##t.goto(100, -60)
+##t.goto(-100, 60)
+##t.goto(-100, 0)
+##t.goto(0, 0)
+##turtle.done()
+##-------------------------------------
+##import turtle
+##turtle.write("test")
+##t = turtle.Turtle()
+##t.setheading(90)
+##t.forward(100)
+##t.setheading(215)
+##t.forward(150)
+##t.setheading(90)
+##t.forward(100)
+##t.home()
+##-------------------------------------
+##import turtle
+##t = turtle.Turtle()
+##t.fd(100)
+##t.lt(90)
+##t.fd(100)
+##t.lt(90)
+##t.fd(100)
+##t.lt(90)
+##t.fd(100)
+##-------------------------------------
+##import turtle
+##t = turtle.Turtle()
+
+##t.rt(90)
+##t.fd(100)
+##t.rt(60)
+##t.bk(100)
+##t.rt(60)
+##t.fd(100)
+##
+##turtle.done()
+##-------------------------------------
+##t.shape("turtle")
+##t.speed(0)
+##t.fd(100)
+##t.pencolor("green")
+##t.lt(120)
+##t.fd(100)
+##t.pensize(5)
+##t.lt(120)
+##t.fd(100)
+##t.penup()
+##t.goto(-50, 86.6)
+##turtle.addshape("new_turtle.gif")
+##t.shape("new_turtle.gif")
+##t.pendown()
+##t.pen(pensize = 10, pencolor = "red", speed = 10)
+##t.setheading(330)
+##t.fd(86.6)
+##turtle.done()
+##-------------------------------------
+##t.speed(1)
+##t.rt(90)
+##stmp1 = t.stamp()
+##t.fd(30)
+##stmp2 = t.stamp()
+##t.fd(50)
+##t.undo()
+##t.clearstamp(stmp1)
+##t.penup()
+##t.goto(-20, 0)
+##t.dot(10)
+##t.goto(20 ,0)
+##t.dot(10)
+##t.goto(15, -40)
+##t.rt(75)
+##t.pendown()
+##t.circle(-50, 45)
+##turtle.done()
+##-------------------------
+##t.shape('turtle')
+##
+##r = 50
+##t.circle(r)
+##t.goto(100, 0)
+##
+##r = 80
+##t.circle(r)
+##t.goto(200, 0)
+##
+##r = 110
+##t.circle(r)
+##-------------------------
+##import turtle
+##t = turtle.Turtle()
+##t.goto(100, 0)
+##t.goto(100, -30)
+##t.goto(-100, 30)
+##t.goto(-100, 0)
+##t.goto(0, 0)
+##t.goto(0, 100)
+##t.goto(30, 100)
+##t.goto(-30, -100)
+##t.goto(0, -100)
+##t.goto(0, 0)
+##
+##turtle.home()
+##-------------------------
+
+##import turtle
+##t = turtle.Turtle()
+##
+##r = 50
+##t.circle(r)
+##t.penup()
+##t.goto(50, 0)
+##
+##t.pendown()
+##r = r + 30
+##t.circle(r)
+##t.penup()
+##t.goto(100, 0)
+##
+##t.pendown()
+##r = r + 30
+##t.circle(r)
+##-------------------------
+##import turtle
+##t = turtle.Turtle()
+##
+##a = int(input("집의 크기는?  : "))
+##t.goto(a, 0)
+##t.goto(a/2, a)
+##t.goto(0, 0)
+##t.goto(0, -a)
+##t.goto(a, -a)
+##t.goto(a, 0)
+##t.goto(0, 0)
+##-------------------------
+##import turtle
+##t = turtle.Turtle()
+##
+##t.goto(200, 0)
+##t.goto(60, -120)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

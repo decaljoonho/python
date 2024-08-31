9660ebe2cf8ecd3cce8496cffcf2f22af3e5d9e8
decaljoonho <161907483+decaljoonho@users.noreply.github.com>
2024-08-31 11:40:05 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ width, height = 745,374
 
 win = Tk()
 win.title("Tennis Game")
-win.geometry("725x374+150+150")
+win.geometry("745x374+150+150")
 win.resizable(False,False)
 court = Court(win, 745,374,"court.png")
 
@@ -17,29 +17,40 @@ x2,y2 = x1+30, y1+30
 ball = Ball(court,x1,y1,x2,y2)
 
 def play_game() :
+    if ball.x1 <= 5 :
+        winner = "green"
+
+    if ball.x1 + ball.width >= court.width - 5 :
+        winner = "red"
     ball.move_ball()
     win.after(50,play_game)
+
+
+
+def score() :
+    global green_score,red_score,winner
+
+    if winner == "red" :
+        red_score += 1
+        winner = ""
+
+    elif winner == "green" :
+        green_score += 1
+        winner = ""
+    court.draw_score(red_score,green_score)
+
+def restart(event) :
+    score()
+    red_racket.start_position()
+    green_racket.start_position()
+    ball.start_ball()
+
+
+
+
+
 play_game()
-
-
 win.mainloop()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-win.mainloop()
     
 

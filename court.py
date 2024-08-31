9095d7b45b177ed6b26fd372dfba6be8e1bb54d1
decaljoonho <161907483+decaljoonho@users.noreply.github.com>
2024-08-31 12:02:31 +0900

    완성

--- court.py
+++ court.py
@@ -4,6 +4,7 @@ class Court :
 
     def __init__(self, window, width, height, img) :
         self.canvas = Canvas(window, width = width, height = height)
+
         self.img = PhotoImage(file = img)
         self.canvas.create_image(width/2, height/2, image = self.img)
 
@@ -11,3 +12,12 @@ class Court :
         self.height = height
 
         self.canvas.pack()
+
+        my_font = ("consolas", 50)
+        self.board = self.canvas.create_text(width/2,65,font = my_font,fill = "white")
+
+    def draw_score(self, score1, score2) :
+        scores = str(score1) + " : " + str(score2)
+
+        self.canvas.itemconfigure(self.board,text = scores)
+        
